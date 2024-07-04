c968726ba2102c2c96aa313a7ea38b5c264a00d9
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-04 11:23:59 +1000

    Created users.service and users.controllers with basic routes.

--- tome-server/src/users/users.controller.ts
+++ tome-server/src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+  Body,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './../entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    return this.usersService.create(user);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() user: User) {
+    return this.usersService.update(+id, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
--- tome-server/src/users/users.service.ts
+++ tome-server/src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async create(user: User): Promise<User> {
+    return this.usersRepository.save(user);
+  }
+
+  async update(id: number, user: User): Promise<User> {
+    await this.usersRepository.update(id, user);
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+}
