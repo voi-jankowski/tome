1ab4df8d5072a992082eb28d966f3d59e48cbd36
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-04 21:10:17 +1000

    Created projects.module, projects.service and projects.controllers with basic routes.

--- tome-server/src/projects/projects.controller.ts
+++ tome-server/src/projects/projects.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+  Body,
+} from '@nestjs/common';
+import { ProjectsService } from './projects.service';
+import { Project } from './../entities/project.entity';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.projectsService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() project: Project) {
+    return this.projectsService.create(project);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() project: Project) {
+    return this.projectsService.update(+id, project);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.projectsService.remove(+id);
+  }
+}
--- tome-server/src/projects/projects.service.ts
+++ tome-server/src/projects/projects.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Project } from '../entities/project.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private projectsRepository: Repository<Project>,
+  ) {}
+
+  findAll(): Promise<Project[]> {
+    return this.projectsRepository.find();
+  }
+
+  findOne(id: number): Promise<Project> {
+    return this.projectsRepository.findOneBy({ id });
+  }
+
+  async create(project: Project): Promise<Project> {
+    return this.projectsRepository.save(project);
+  }
+
+  async update(id: number, project: Project): Promise<Project> {
+    await this.projectsRepository.update(id, project);
+    return this.projectsRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.projectsRepository.delete(id);
+  }
+}
