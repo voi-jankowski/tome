185e8c9f8af1a17afc6cd8f2752beeac6616e702
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-05 11:14:05 +1000

    Updated user.service to create default categories and attributes for new user.

--- tome-server/src/users/users.service.ts
+++ tome-server/src/users/users.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/entities/category.entity';
+import { Attribute, AttributeType } from 'src/entities/attribute.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>,
+    @InjectRepository(Attribute)
+    private attributesRepository: Repository<Attribute>,
   ) {}
 
   findAll(): Promise<User[]> {
@@ -19,7 +25,9 @@ export class UsersService {
   }
 
   async create(user: User): Promise<User> {
-    return this.usersRepository.save(user);
+    const newUser = await this.usersRepository.save(user);
+    await this.createDefaultCategoriesAndAttributes(newUser);
+    return newUser;
   }
 
   async update(id: number, user: User): Promise<User> {
@@ -29,5 +37,128 @@ export class UsersService {
 
   async remove(id: number): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  private async createDefaultCategoriesAndAttributes(
+    user: User,
+  ): Promise<void> {
+    const defaultCategories = [
+      { name: 'Characters', isDefault: true, user },
+      { name: 'Places', isDefault: true, user },
+      { name: 'Events', isDefault: true, user },
+    ];
+
+    const categories = [];
+    for (const categoryData of defaultCategories) {
+      const category = this.categoriesRepository.create(categoryData);
+      categories.push(await this.categoriesRepository.save(category));
+    }
+
+    const defaultAttributes = {
+      Characters: [
+        {
+          name: 'name',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[0],
+        },
+        {
+          name: 'DOB',
+          type: AttributeType.DATE,
+          isDefault: true,
+          user,
+          category: categories[0],
+        },
+        {
+          name: 'description',
+          type: AttributeType.TEXT,
+          isDefault: true,
+          user,
+          category: categories[0],
+        },
+        {
+          name: 'home',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[0],
+        },
+      ],
+      Places: [
+        {
+          name: 'name',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[1],
+        },
+        {
+          name: 'location',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[1],
+        },
+        {
+          name: 'description',
+          type: AttributeType.TEXT,
+          isDefault: true,
+          user,
+          category: categories[1],
+        },
+        {
+          name: 'characters involved',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[1],
+        },
+      ],
+      Events: [
+        {
+          name: 'name',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[2],
+        },
+        {
+          name: 'location',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[2],
+        },
+        {
+          name: 'time',
+          type: AttributeType.DATETIME,
+          isDefault: true,
+          user,
+          category: categories[2],
+        },
+        {
+          name: 'description',
+          type: AttributeType.TEXT,
+          isDefault: true,
+          user,
+          category: categories[2],
+        },
+        {
+          name: 'characters involved',
+          type: AttributeType.STRING,
+          isDefault: true,
+          user,
+          category: categories[2],
+        },
+      ],
+    };
+
+    for (const categoryAttributes of Object.values(defaultAttributes)) {
+      for (const attributeData of categoryAttributes) {
+        const attribute = this.attributesRepository.create(attributeData);
+        await this.attributesRepository.save(attribute);
+      }
+    }
   }
 }
