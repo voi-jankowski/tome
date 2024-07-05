1b3de7812f9fc3d5ba9924a775ba6c5c031b5401
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-05 18:43:12 +1000

    Created module, service and controllers with basic routes for attributes.

--- tome-server/src/attributes/attributes.controller.ts
+++ tome-server/src/attributes/attributes.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+  Body,
+  Query,
+  Request,
+} from '@nestjs/common';
+import { AttributesService } from './attributes.service';
+import { Attribute } from './../entities/attribute.entity';
 
 @Controller('attributes')
-export class AttributesController {}
+export class AttributesController {
+  constructor(private readonly attributesService: AttributesService) {}
+
+  @Get('category/:categoryId')
+  findAllByCategory(@Param('categoryId') categoryId: number) {
+    return this.attributesService.findAllByCategory(categoryId);
+  }
+
+  @Get('defaults')
+  findAllDefaultsByUser(@Request() req) {
+    return this.attributesService.findAllDefaultsByUser(req.user.id);
+  }
+
+  @Get('non-defaults')
+  findAllNonDefaultsByUser(@Request() req) {
+    return this.attributesService.findAllNonDefaultsByUser(req.user.id);
+  }
+
+  @Get('search-by-name')
+  findAttributeByName(@Request() req, @Query('name') name: string) {
+    return this.attributesService.findAttributeByName(req.user.id, name);
+  }
+
+  @Get(':id')
+  findAttributeById(@Param('id') id: string) {
+    return this.attributesService.findAttributeById(+id);
+  }
+
+  @Post()
+  create(@Request() req, @Body() attribute: Attribute) {
+    attribute.user = req.user;
+    return this.attributesService.create(attribute);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() attribute: Attribute) {
+    return this.attributesService.update(+id, attribute);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.attributesService.remove(+id);
+  }
+}
--- tome-server/src/attributes/attributes.service.ts
+++ tome-server/src/attributes/attributes.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { Attribute } from '../entities/attribute.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AttributesService {}
+export class AttributesService {
+  constructor(
+    @InjectRepository(Attribute)
+    private attributesRepository: Repository<Attribute>,
+  ) {}
+
+  findAllByCategory(categoryId: number): Promise<Attribute[]> {
+    return this.attributesRepository.find({
+      where: { category: { id: categoryId } },
+    });
+  }
+
+  findAllDefaultsByUser(userId: number): Promise<Attribute[]> {
+    return this.attributesRepository.find({
+      where: { isDefault: true, user: { id: userId } },
+    });
+  }
+
+  findAllNonDefaultsByUser(userId: number): Promise<Attribute[]> {
+    return this.attributesRepository.find({
+      where: { isDefault: false, user: { id: userId } },
+    });
+  }
+
+  findAttributeByName(
+    userId: number,
+    name: string,
+  ): Promise<Attribute | undefined> {
+    return this.attributesRepository.findOne({
+      where: { name, user: { id: userId } },
+    });
+  }
+
+  findAttributeById(id: number): Promise<Attribute | undefined> {
+    return this.attributesRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async create(attribute: Attribute): Promise<Attribute> {
+    // Check for duplicate attribute
+    const existingAttribute = await this.findAttributeByName(
+      attribute.user.id,
+      attribute.name,
+    );
+    if (existingAttribute) {
+      throw new ConflictException('Attribute with this name already exists.');
+    }
+    return this.attributesRepository.save(attribute);
+  }
+
+  async update(id: number, attribute: Attribute): Promise<Attribute> {
+    await this.attributesRepository.update(id, attribute);
+    return this.attributesRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.attributesRepository.delete(id);
+  }
+}
