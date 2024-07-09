41930cd07afbfc8480c9601351f34b675557a4c9
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-09 17:09:48 +1000

    Applied Auth guard to the users.controller.

--- tome-server/src/users/users.controller.ts
+++ tome-server/src/users/users.controller.ts
@@ -6,41 +6,47 @@ import {
   Put,
   Delete,
   Body,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './../entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  // Public route: registration
+  @Post('register')
+  create(@Body() user: User) {
+    return this.usersService.create(user);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
+  // Public route: find user by username (for login process)
   @Get('username/:username')
   findOneByUsername(@Param('username') username: string) {
     return this.usersService.findOneByUsername(username);
   }
 
-  @Post()
-  create(@Body() user: User) {
-    return this.usersService.create(user);
+  // Protected route: get logged-in user details
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req) {
+    return this.usersService.findOne(req.user.id);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() user: User) {
-    return this.usersService.update(+id, user);
+  // Protected route: update logged-in user details
+  @UseGuards(JwtAuthGuard)
+  @Put('me')
+  updateProfile(@Request() req, @Body() user: User) {
+    return this.usersService.update(req.user.id, user);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  // Protected route: delete logged-in user
+  @UseGuards(JwtAuthGuard)
+  @Delete('me')
+  removeProfile(@Request() req) {
+    return this.usersService.remove(req.user.id);
   }
 }
